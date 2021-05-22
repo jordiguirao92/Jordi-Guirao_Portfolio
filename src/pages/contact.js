0a1015024b68d77f1da94dcbb155d9c80e5ee968
jordiguirao92 <jordiguirao92@users.noreply.github.com>
2021-05-22 14:07:16 +0200

    Email Errors

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,13 @@
 import { useState} from 'react';
 import Slide from 'react-reveal/Slide';
 import emailjs from 'emailjs-com';
+import Alert from '@material-ui/lab/Alert';
 
 import MainLayout from '../componets/layout/MainLayout'
 import {Button, Input, Label, TextArea, P, Spacer, Flex} from '../styles/ui';
-const {REACT_APP_SERVICEID, REACT_APP_TEMPLATEID, REACT_APP_USERID } = process.env
+const {REACT_APP_SERVICEID, REACT_APP_TEMPLATEID, REACT_APP_USERID } = process.env;
+
+
 
 
 
@@ -14,20 +17,26 @@ const ContactPage = () => {
         email: '',
         message: ''
     });
-    const [error, setError] = useState('');
+    const [emailSuccess, setEmailSuccess] = useState(false);
+    const [errorEmail, setErrorEmail] = useState(false);
+    const [errorInput, setErrorInput] = useState(false);
 
     const sendEmail = (event) => {
         event.preventDefault();
+        setEmailSuccess(false);
+        setErrorEmail(false);
+        setErrorInput(false);
         if(formData.name && formData.email && formData.message) {
-            setError('');
             emailjs.sendForm(REACT_APP_SERVICEID, REACT_APP_TEMPLATEID, event.target, REACT_APP_USERID)
             .then((result) => {
                 console.log(result.text);
+                setEmailSuccess(true);
             }, (error) => {
                 console.log(error.text);
+                setErrorEmail(true);
             });
         } else {
-            setError('Please fill all fields, thanks :)')
+            setErrorInput(true, 'Please fill all fields, thanks :)')
         } 
       };
   
@@ -76,7 +85,10 @@ const ContactPage = () => {
                                     setFormData({...formData, message: event.target.value });}}
                             />
                     <Spacer height='30px'/>
-                    {error && <P color='red'>&nbsp;{error}</P>}
+                    {errorEmail && <Alert severity="error">&nbsp;Sorry, a problem has occurred. Try again or contact me by LinkedIn or jordigm92@gmail.com</Alert>}
+                    {errorInput && <Alert severity="error">&nbsp;Please fill all fields, thanks :)</Alert>}
+                    {emailSuccess && <Alert severity="success">&nbsp;Thanks for contact me. I will answer as soon as possible</Alert>}
+                    <Spacer height='10px'/>
                     <Button>Contact me</Button>   
                 </form>
                 
